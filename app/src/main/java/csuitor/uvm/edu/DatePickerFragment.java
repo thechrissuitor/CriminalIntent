b967b96040767db10c581eadc15c15420a05492b
Christopher Thomas Suitor <christopher.suitor@uvm.edu>
2019-09-30 17:58:56 -0400

    Have completed and commented through page 257.

--- app/src/main/java/csuitor/uvm/edu/DatePickerFragment.java
+++ app/src/main/java/csuitor/uvm/edu/DatePickerFragment.java
@@ -2,6 +2,8 @@ package csuitor.uvm.edu;
 
 import android.app.Dialog;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.DialogFragment;
@@ -10,8 +12,12 @@ public class DatePickerFragment extends DialogFragment {
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Inflate the view. Later, .setView(v) sets the view on the dialog
+        View v = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_date, null);
+
         // build an AlertDialog with a title and one OK button
         return new AlertDialog.Builder(getActivity())
+                .setView(v)
                 .setTitle(R.string.date_picker_title)
                 .setPositiveButton(android.R.string.ok, null)
                 .create();
